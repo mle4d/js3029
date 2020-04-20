77a5fe43476fa6eef564f9632e2e863c8cb53926
emily <mle4d@me.com>
2020-04-19 20:10:20 -0700

    takeout end time since not using

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
-const endTime = document.querySelector('.display__end-time');
 const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
-  // clear any existing timers
   clearInterval(countdown);
 
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
-  displayEndTime(then);
+
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
-    // check if we should stop it!
     if(secondsLeft < 0) {
       clearInterval(countdown);
       return;
     }
-    // display it
     displayTimeLeft(secondsLeft);
   }, 1000);
 }
@@ -32,12 +28,6 @@ function displayTimeLeft(seconds) {
   timerDisplay.textContent = display;
 }
 
-function displayEndTime(timestamp) {
-  const end = new Date(timestamp);
-  const hour = end.getHours();
-  const adjustedHour = hour > 12 ? hour - 12 : hour;
-  const minutes = end.getMinutes();
-}
 
 function startTimer() {
   const seconds = parseInt(this.dataset.time);
@@ -48,7 +38,6 @@ buttons.forEach(button => button.addEventListener('click', startTimer));
 document.customForm.addEventListener('submit', function(e) {
   e.preventDefault();
   const mins = this.minutes.value;
-  console.log(mins);
   timer(mins * 60);
   this.reset();
 });
